c1f03c56fd7c37eeb84b012bc3fce576c6ee8886
gisungchoi <chlrltjd159@naver.com>
2022-05-09 00:41:58 +0900

    feat : AUTO 전략
    - auto는 선택한 데이터베이스 방언에 따라 자동으로 선택되는 것이다
    - mySql이면 IDENTITY를 사용한다

--- src/main/kotlin/com/jpa/book/chapter4/Person.kt
+++ src/main/kotlin/com/jpa/book/chapter4/Person.kt
@@ -8,7 +8,7 @@ import javax.persistence.Id
 @Entity
 class Person(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long,
 ) {
 }
