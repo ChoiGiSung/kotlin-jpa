633115e8ebf756828927f22fd8f4993cfb365235
gisungchoi <chlrltjd159@naver.com>
2022-05-10 03:25:31 +0900

    feat : FACADE 계층 추가
    - 프레젠테이션 계층에서 쓸 정보를 DB에서 불러오기 위해 써본다
    - service 계층은 비즈니스 로직만 있어야 하고 페치 조인을 하게 된다면 데이터 접근 계층과의 의존성이 발생한다
    - 그래서 파사드 계층을 추가해 여기서 접근한다
    - 단점은 계층이 추가된다는 것과 단순 호출만 있을 것이라는 점

---
+++ src/main/kotlin/com/jpa/book/chapter13/OrderFacade.kt
@@ -0,0 +1,16 @@
+package com.jpa.book.chapter13
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.transaction.annotation.Transactional
+
+class OrderFacade(
+    @Autowired
+    val sampleService : SampleService
+) {
+
+    @Transactional
+    fun findOrder(id : Long){
+        //비즈니스 로직
+        sampleService.logic()
+    }
+}
---
+++ src/main/kotlin/com/jpa/book/chapter13/SampleService.kt
@@ -0,0 +1,11 @@
+package com.jpa.book.chapter13
+
+import org.springframework.stereotype.Service
+
+@Service
+class SampleService {
+    fun logic() {
+        //비즈니스 로직
+    }
+
+}
