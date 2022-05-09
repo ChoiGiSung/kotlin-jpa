f9e1190a8f7b6c7aec77a87b507083475aa3328e
gisungchoi <chlrltjd159@naver.com>
2022-05-10 03:08:35 +0900

    feat : 글로벌 페치 전략
    - eager로 했을 경우 jpql로 조회시 n+1 문제가 발생할 수 있음
    - 단순히 jpql을 사용하지 않고 find 했을 때는 문제 x join해서 가지고 오기에

---
+++ src/main/kotlin/com/jpa/book/chapter13/Member.kt
@@ -0,0 +1,27 @@
+package com.jpa.book.chapter13
+
+import javax.persistence.*
+
+@Entity
+class Member(
+    @Id
+    @GeneratedValue
+    val id: Long,
+
+    val username: String,
+
+    team: Team? = null,
+) {
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID", nullable = false)
+    var team: Team? = team
+        set(value) {
+            //무한루프 주의
+            field?.removeMember(this)
+            field = value
+            field?.addMember(this)
+        }
+
+}
---
+++ src/main/kotlin/com/jpa/book/chapter13/Team.kt
@@ -0,0 +1,27 @@
+package com.jpa.book.chapter13
+
+import javax.persistence.*
+
+@Entity
+class Team(
+    @Id
+    @GeneratedValue
+    val id: Long,
+
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+    val members: MutableList<Member> = mutableListOf(),
+
+    val name: String,
+) {
+    fun addMember(member: Member) {
+        //무한루프 주의
+        members.add(member)
+        member.team?.let {
+            if (it != this) member.team = this
+        }
+    }
+
+    fun removeMember(member: Member) {
+        members.remove(member)
+    }
+}
