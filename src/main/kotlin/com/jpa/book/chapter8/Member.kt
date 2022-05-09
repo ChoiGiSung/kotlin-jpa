9c90fcdb489c22563aad412bde2a98fa19da5ca1
gisungchoi <chlrltjd159@naver.com>
2022-05-09 22:05:59 +0900

    feat : 레이지 로딩
    - inner join이 아닌 left join을 사용한다. 없는 경우도 있으니 그렇다
    - 이는 joinColumn에 nallable에 false를 사용하면 innser join을 사용한다

---
+++ src/main/kotlin/com/jpa/book/chapter8/Member.kt
@@ -0,0 +1,26 @@
+package com.jpa.book.chapter8
+
+import javax.persistence.*
+
+@Entity
+class Member(
+    @Id
+    @GeneratedValue
+    val id: Long,
+
+    val username: String,
+
+    team: Team? = null,
+) {
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", nullable = false)
+    var team: Team? = team
+        set(value) {
+            //무한루프 주의
+            field?.removeMember(this)
+            field = value
+            field?.addMember(this)
+        }
+
+}
